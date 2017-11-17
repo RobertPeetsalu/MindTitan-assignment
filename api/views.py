816bd29e555640b2299a28d6161f7c6e55d6864e
Robert Peetsalu <robert.peetsalu@gmail.com>
2017-11-17 11:56:07 +0200

    Richer request error cases

--- api/views.py
+++ api/views.py
@@ -6,41 +6,35 @@
 from api import app, log
 from api.models import predict
 from flask import jsonify, request, make_response, abort
-#import requests
 
 labels = ['price', 'dress_code', 'accessibility', 'parking_lot', 'smoking_area', 'other_services']
+example = ' Here is an example of a correct JSON string with required restaurant features: [{"price":"low", "dress_code":"formal", "accessibility":"completely", "parking_lot":"public", "smoking_area":"not_permitted", "other_services":"internet"}]'
 
 @app.errorhandler( 400 )
-def bad_request( error ):
-    bad_request_hint = 'Correct request format is a JSON list containing following keys: ' + str( labels )
-    return make_response( jsonify( {'bad_request_error': bad_request_hint} ), 400 )
+def not_json( error ):
+    not_json_hint = 'No JSON string was passed.' + example
+    return make_response( jsonify( {'bad_request_error': not_json_hint} ), 400 )
+
+@app.errorhandler( 417 )
+def missing_label( error ):
+    missing_label_hint = 'Some required restaurant features were missing from your JSON string.' + example
+    return make_response( jsonify( {'bad_request_error': missing_label_hint} ), 400 )
+
+@app.errorhandler( 418 )
+def not_text( error ):
+    not_text_hint = 'All restaurant feature values must be strings.' + example
+    return make_response( jsonify( {'bad_request_error': not_text_hint} ), 400 )
 
 @app.route( '/', methods=['GET', 'POST'] )
 def index():
     log.info( 'request received' )
     if not request.json:
-        log.info( 'not JSON' )
         abort( 400 )
-#    for label in labels: #TODO test with bad requests
-#        if label not in request.json:
-#            log.info( 'missing label' )
-#            abort( 400 )
-#        if not isinstance( type( request.json[label] ), str ):
-#            log.info( 'non-text value' )
-#            abort( 400 )
+    for label in labels:
+        if label not in request.json[0].keys():
+            abort( 417 )
+        if not isinstance( request.json[0][label] , str ):
+            log.info( type( request.json[0][label] ) )
+            abort( 418 )
     rating = predict( request.get_json() )
     return jsonify( { 'rating': rating.tolist() } ), 201
-
-#@app.route( '/test', methods=['GET', 'POST'] )
-#def test():
-#    test_restaurant = '[{"price":"low", \
-#                     "dress_code":"formal", \
-#                     "accessibility":"completely", \
-#                     "parking_lot":"public", \
-#                     "smoking_area":"not_permitted", \
-#                     "other_services":"internet"}]'
-#    log.info( 'test request received' )
-#    response = requests.post( 'http://127.0.0.1:5000/', data=test_restaurant )
-#    log.info( 'response.text:' )
-#    log.info( response.text )
-#    return response
